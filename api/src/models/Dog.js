c957c8585fddd61bb3cc9c6bd6c6aef7fa457fcc
Seba <sebastian.pitra10@gmail.com>
2022-11-05 00:05:23 -0300

    relation models and connection with database

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,31 +3,37 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("dog", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      dog_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, //no puedo tener 3 razas igaules en la DB
+      },
+      height: {
+        type: DataTypes.INTEGER, //
+        allowNull: false,
+      },
+      weight: {
+        //peso
+        type: DataTypes.INTEGER, //fijarse despues como es bien en la API
+        allowNull: false,
+      },
+      //esperanza de vida:
+      life_span: {
+        //En la BD no va CamelCase
+        type: DataTypes.STRING,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true, //no puedo tener 3 razas igaules en la DB
-    },
-    height: {
-      type: DataTypes.INTEGER, //
-      allowNull: false,
-    },
-    weight: {
-      //peso
-      type: DataTypes.INTEGER, //fijarse despues como es bien en la API
-      allowNull: false,
-    },
-    //esperanza de vida:
-    life_span: {
-      //En la BD no va CamelCase
-      type: DataTypes.STRING,
-    },
-  });
+    {
+      timestamps: false, //no aparece createdAt y updatedAt
+    }
+  );
 };
--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -1,14 +1,21 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("temperament", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
+  sequelize.define(
+    "temperament",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      unique: true,
-    },
-  });
+    {
+      timestamps: false, //no aparece createdAt y updatedAt
+    }
+  );
 };
