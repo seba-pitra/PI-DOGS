1603f9de8b647682535689596f4a241a0cf8929d
Seba <sebastian.pitra10@gmail.com>
2022-11-22 16:23:11 -0300

    add script start

--- api/api/index.js
+++ api/api/index.js
@@ -3,7 +3,7 @@ const { conn } = require("../src/db.js");
 
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log("%s listening at 3001");
   });
 });
 
